6c4d16b3c7355e3e75b4b5e9c38c29cbe613b439
cebollinos11 <cebollinos@gmail.com>
2016-01-30 19:16:30 +0100

    improved halo

--- Assets/Scripts/PentagramoDisplay.cs
+++ Assets/Scripts/PentagramoDisplay.cs
@@ -4,22 +4,34 @@ using System.Collections;
 public class PentagramoDisplay : MonoBehaviour {
 
 	public Pentagramo pentagramo;
-
+    public GameObject halo;
+    public ParticleSystem particles;
 	public GameObject dullFace;
 	public GameObject glowingFace;
 
 	bool isFading;
 	// Use this for initialization
 	void Start () {
-	
+        halo.SetActive(false);
 	}
-	
+
+
+    public void StartParticles() {
+        particles.Play();
+    }
+
+    public void StopParticles() {
+        particles.Stop();
+    }
+
 	// Update is called once per frame
 	void Update () {
 
 		if(pentagramo.state == Pentagramo.State.Glowing){
 			glowingFace.SetActive(true);
+            halo.SetActive(true);
 			dullFace.SetActive(false);
+            
 		}
 		else if(pentagramo.state == Pentagramo.State.Fading){
 			if(!isFading){
@@ -31,7 +43,9 @@ public class PentagramoDisplay : MonoBehaviour {
 			isFading = false;
 			CancelInvoke("pulse");
 			glowingFace.SetActive (false);
+            halo.SetActive(false);
 			dullFace.SetActive(true);
+            
 		}
 
 	
