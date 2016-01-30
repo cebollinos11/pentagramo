38449c8f985ed2c5c437eb4c2bbce6c38f949ab6
cebollinos11 <cebollinos@gmail.com>
2016-01-30 11:44:59 +0100

    test2

--- Assets/Actor.cs
+++ Assets/Actor.cs
@@ -5,8 +5,12 @@ public class Actor : MonoBehaviour {
 
 
     GameObject player;
-    Rigidbody rB;
-
+    
+    Vector3 currentTarget;
+    public float speed;
+    public float wanderRange;
+    public float wanderFrequencyChange;
+    float currentWanderFrequencyTimer;
     public enum aI_states { 
     
         follow
@@ -17,17 +21,45 @@ public class Actor : MonoBehaviour {
 	void Start () {
 
         player = GameObject.FindGameObjectWithTag("Player");
-        rB = GetComponent<Rigidbody>();
+        FindNewRandomTarget();
+        
 	    
 	}
+
+    void FindNewRandomTarget() {
+
+        currentTarget = transform.position + new Vector3(Random.Range(-wanderRange, wanderRange), 0f, Random.Range(-wanderRange, wanderRange));
+    
+    }
+
+    void Move(Vector3 dir) {
+
+        dir *= 0.0001f;
+        dir *= speed;
+        transform.position = new Vector3(transform.position.x + dir.x, transform.position.y, transform.position.z + dir.z);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
         Vector3 dir;
-        dir = player.transform.position - transform.position;
+        dir = currentTarget - transform.position;
         dir = dir.normalized;
-        
+        Move(Vector3.Scale((currentTarget - transform.position), new Vector3(1, 0, 1)));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            FindNewRandomTarget();
+        }
+
+        currentWanderFrequencyTimer -= Time.deltaTime;
+        if (currentWanderFrequencyTimer < 0)
+        {
+            currentWanderFrequencyTimer = wanderFrequencyChange + Random.Range(0, 1f);
+            FindNewRandomTarget();
+        }
 	
 	}
 }
