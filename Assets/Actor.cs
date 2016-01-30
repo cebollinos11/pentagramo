0aaf385c5f4a781222de72ff1b603835467bc666
cebollinos11 <cebollinos@gmail.com>
2016-01-30 11:17:56 +0100

    test

---
+++ Assets/Actor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor : MonoBehaviour {
+
+
+    GameObject player;
+    Rigidbody rB;
+
+    public enum aI_states { 
+    
+        follow
+    
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        rB = GetComponent<Rigidbody>();
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 dir;
+        dir = player.transform.position - transform.position;
+        dir = dir.normalized;
+        
+	
+	}
+}
